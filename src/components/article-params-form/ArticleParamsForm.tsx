957c4c01e2c8a4168924be7491cafb86e922eaec
Maxim Osipov <oasipovmak@yandex.ru>
2026-06-13 14:03:15 +0300

    Добавлен выбор шрифта и размер шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,79 @@
+import { useState, useRef, useEffect } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group';
+
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const formRef = useRef<HTMLDivElement>(null);
+
+	const [fontFamily, setFontFamily] = useState<OptionType>(
+		fontFamilyOptions[0]
+	);
+	const [fontSize, setFontSize] = useState<OptionType>(fontSizeOptions[0]);
+
+	const toggleForm = () => {
+		setIsOpen(!isOpen);
+	};
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (formRef.current && !formRef.current.contains(event.target as Node)) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+		<div ref={formRef}>
+			<ArrowButton isOpen={isOpen} onClick={toggleForm} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Separator />
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={setFontFamily}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={fontSize}
+						onChange={setFontSize}
+						title='Размер шрифта'
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
